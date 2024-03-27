9f0b8b22444ff7bbd98dd1aeaec91b14a700b6a9
cipher <120999508+Primi373-creator@users.noreply.github.com>
2024-03-27 21:40:27 +0000

    new cai

--- config.js
+++ config.js
@@ -12,23 +12,44 @@ const config = {
     sasuke: {
       id: "a89f2n10nc1c2c9d1b2a3c4",
     },
+    sasuke: {
+      id: "a89f2n10nc1c2c9d1b2a3c4",
+    },
+    sasuke: {
+      id: "a89f2n10nc1c2c9d1b2a3c4",
+    },
+    eminenceinsha: {
+      id: "L6E38h1bIPYjR8Xz0oRE2JEcnyicPfFIx936CsS4fyg",
+    },
+    nanami: {
+      id: "Rt_VSucKURXkCuN_5vMYuSNtPSGtFaH4YCcx9_RSRj0",
+    },
+    benimaru: {
+      id: "4c938qxqZ6jVhkGHEgZMjo4eq-_d6d6ruJZDjwAoMg4",
+    },
+    shadowgarden: {
+      id: "5xR5pGO-SNySTUY6jNIGm8zKDLzYJg-p9CUBa2QHi0s",
+    },
+    zerotwo: {
+      id: "ql8RTLD8EmJNH4cDJr2n91RohXLo5_Aw0GMMCaZQeNg",
+    },
     gojo: {
-      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+      id: "4WOVrCApi4JYwfYwU2e5eDeFalLOkGBw6IfUZPX1XVQ",
     },
     delta: {
-      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+      id: "FeJX2Cgn2Mnn_83b9QiJEbBLKh-f9C_n9hd9-2hliNs",
     },
-    gojo: {
-      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    power: {
+      id: "TT2Ac7tCK2jVT9PZKElYrBQVNAmTLC2BglvOap6R_04",
     },
-    gojo: {
-      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    cidkagenou: {
+      id: "ab1vLCBGWkSd09ZTOr7RSsOo7RzvJ0ho--FGLxoxQOo",
     },
-    gojo: {
-      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    shadow: {
+      id: "j4bccfB3sGqEl2tvSvqJZZ_GOVixcIX2sTJTWWcZGK8",
     },
-    gojo: {
-      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    alpha: {
+      id: "rXLTGQ5ojO0gx2Uwv2EoD_prbT25bfu0hnlb_WC8sxk",
     },
   },
 };
