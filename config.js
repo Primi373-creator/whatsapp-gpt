0399b1887e04e8e06119846e0b24b8ac63efe4f0
cipher <120999508+Primi373-creator@users.noreply.github.com>
2024-03-27 21:30:14 +0000

    hey

---
+++ config.js
@@ -0,0 +1,36 @@
+const config = {
+  port: 3000,
+  baseurl: "http://example.com",
+  alphaurl: "http://alpha.example.com",
+  basekey: "",
+  alphakey: "",
+  prefix: "",
+  characterai: {
+    naruto: {
+      id: "lgrcnqyrnpxgfucpo8yccpq",
+    },
+    sasuke: {
+      id: "a89f2n10nc1c2c9d1b2a3c4",
+    },
+    gojo: {
+      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    },
+    delta: {
+      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    },
+    gojo: {
+      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    },
+    gojo: {
+      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    },
+    gojo: {
+      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    },
+    gojo: {
+      id: "b2a9f8n1c2nc2n1n3a8a8c2",
+    },
+  },
+};
+
+module.exports = config;
